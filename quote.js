d7657848e676e8eea4c84e4b87fcce3547e5d653
Kanchan Lawoju <lawojukanchan@gmail.com>
2025-12-11 12:08:31 +0545

    first commit

---
+++ quote.js
@@ -0,0 +1,56 @@
+const quotes={
+    success:[
+        {
+            text:"Success is going from failure to failure without losing your enthusiasm",
+            author:"Winston Churchill"
+        },
+        {
+            text:"I never dreamed about sucess.I worked for it",author:"Estee Lauder"
+        },
+        {
+            text:"Sucess is the sum of small efforts-repeated day in and day out",author:"Robert Collier"
+        },
+        {
+            text:"The path to success is to take massive,determined action",author:"Tony Robbins"
+        }
+        ],
+       motivational:[
+        {
+            text:"You have to believe in yourself when no one else does",author:"Serena Williams"
+        },
+        {
+            text:"Start where you are.Use what you have.Do what you can",author:"Arthur Ashe"
+        },
+        {
+            text:"Learn from yesterday,Live for today,Hope for tommorow",author:"Albert Einstein"
+        }
+       ] ,
+       education:[
+        {
+            text:"Education is the most powerful weapon which you can use to change the world",author:"Nelson Mandela"
+        },
+        {
+            text:"Intelligence plus character that is the goal of true education",author:"Dr.Martin Luther"
+        },
+        {
+            text:"Education is one thing no one can take away from you",author:"Elin Nordegren"
+        }
+       ],
+       music:[
+        {
+            text:"People haven't always been there for me but music always has",author:"Tayloe Swift"
+        },
+        {
+            text:"Where words fails,Music speaks",author:"Hans Christian Andersen"
+        },
+        {
+            text:"Sometimes music is the only medicine the heart and soul need",author:"John A.Logan"
+        },
+        {
+            text:"One good things about music,when it hits you,you feel no pain",author:"Bob Marley"
+        }
+       ]
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,74 @@
+const themeToggle = document.getElementById('toggle-theme');
+themeToggle.onchange = () => {
+    document.body.classList.toggle("dark");
+};
+ 
+//quotes
+let selectedCategory = "success";
+let index = 0;
+
+function getCurrentQuotes() {
+    return quotes[selectedCategory];  
+}
+
+function showQuotes() {
+    const list = getCurrentQuotes();
+
+    document.getElementById("quote-text").textContent = "“" + list[index].text + "”";
+    document.getElementById("quote-author").textContent = "- " + list[index].author;
+    document.getElementById("quote-count").textContent = `# ${index + 1} of ${list.length}`;
+}
+
+showQuotes();
+
+
+document.getElementById("category").onchange = function() {
+    selectedCategory = this.value;
+    index = 0;
+    showQuotes();
+};
+
+
+document.getElementById("previous").onclick = () => {
+    const list = getCurrentQuotes();
+    index = (index - 1 + list.length) % list.length;
+    showQuotes();
+};
+
+
+document.getElementById("random").onclick = () => {
+    const list = getCurrentQuotes();
+    index = Math.floor(Math.random() * list.length);
+    showQuotes();
+};
+
+
+document.getElementById("next").onclick = () => {
+    const list = getCurrentQuotes();
+    index = (index + 1) % list.length;
+    showQuotes();
+};
+
+// font 
+
+let currentFontSize=24;
+const quoteText=document.getElementById("quote-text");
+
+document.getElementById("font-large").onclick=()=>{
+    currentFontSize +=2;
+    quoteText.style.fontSize =currentFontSize + "px"
+}
+
+document.getElementById("font-normal").onclick=()=>{
+    currentFontSize = 24;
+    quoteText.style.fontSize =currentFontSize + "px"
+}
+
+document.getElementById("font-small").onclick=()=>{
+    currentFontSize -=2;
+    quoteText.style.fontSize =currentFontSize + "px"
+}
+
+
+
+
